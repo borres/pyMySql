c3d7a581dd9b99c9f5e81dd69642aa2a24808471
borres <borre.stenseth@gmail.com>
2013-03-28 08:25:14 +0100

    Create dump2Json.py
    using utils.py

---
+++ dump2Json.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+import cgi,sys
+import subprocess
+from lxml import etree
+import utils
+#import cgitb; cgitb.enable()
+"""
+Dump to json
+using mysqldump in a subprocess as implemented in utils
+"""
+
+XSLT_FILE='tojson.xsl'       
+
+form=cgi.FieldStorage()
+"""
+ Need connect. | separated string
+ Sequence is important:
+ user|password|host|database|table
+"""
+print 'Content-type: text/plain; charset=utf-8 \n'
+
+try:
+    # always connect
+    connect=form['connect'].value
+    # set up parameters
+    p=utils.prepareDumpParameters(connect)
+    parts=connect.split('|')
+    if len(p) > 0:                           
+        print utils.makeJson(p,'data/dump.xml',XSLT_FILE)
+    else:
+        print 'error bad parameter list in connect'
+except:
+        res=sys.exc_info()
+        print 'error '+str(res[1])
